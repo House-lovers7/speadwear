897077f20f2ca3a5a620e1893296f6069d2f74e7
Taisuke Goto <gt@TaisukenoMacBook-Pro.local>
2021-03-05 22:06:46 +0900

    create user and session controller view helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,239 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: %i[edit update destroy following followers]
+  before_action :correct_user, only: %i[edit update]
+  before_action :check_guest, only:  %i[destroy update]
+
+  def index
+    # @users = User.where(activated: true)
+    @cordinate = Cordinate.where(user_id: params[:user_id])
+    @item = Item.where(user_id: params[:user_id])
+    @users = User.all
+    user_paginate
+    cordinate_ransack_setup
+    item_ransack_setup
+
+    # cordinate_ransack_setup
+    # item_ransack_setup
+  end
+
+  def following
+    @title = 'Following'
+    @user = User.find(params[:id])
+    @users = @user.following
+    render 'show_follow'
+  end
+
+  def followers
+    @title = 'Followers'
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follow'
+  end
+
+  def blocking
+    @title = 'blocking'
+    @user = User.find(params[:id])
+    @users = @user.blocking
+    render 'show_follow'
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    authorize! :create, @user, message: 'ユーザーを作成する権限がありません。'
+    if @user.save
+      # @user.send_activation_email
+      UserMailer.account_activation(@user).deliver_now
+      flash[:info] = 'Please check your email to activate your account.'
+      redirect_to @user
+    else
+      redirect_to request.referer
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    authorize! :delete, @user, message: 'ユーザーを削除する権限がありません。'
+    @user.destroy
+  end
+
+  # なぜかErrorがでる
+  # def edit
+  #   @user = User.find(params[:id]
+  # end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  #  Cordinateのransackの設定------------
+
+  def all_cordinate_season_search
+    cordinate_season_ransack
+  end
+
+  def all_cordinate_tpo_search
+    cordinate_tpo_ransack
+  end
+
+  def all_cordinate_rating_search
+    cordinate_rating_ransack
+  end
+
+  def cordinate_ransack_setup
+    cordinate_season_ransack
+    cordinate_tpo_ransack
+    all_cordinate_rating_search
+  end
+
+  def cordinate_season_ransack
+    p_cordinate_season = Cordinate.ransack(params[:p_season], search_key: :p_season)
+    @p_cordinate_season = p_cordinate_season.result
+    @cordinate = Cordinate.where(user_id: current_user.id) if params[:p_season].blank?
+  end
+
+  def cordinate_tpo_ransack
+    p_cordinate_tpo = Cordinate.ransack(params[:p_tpo], search_key: :p_tpo)
+    @p_cordinate_tpo = p_cordinate_tpo.result
+    @cordinate = Cordinate.where(user_id: current_user.id) if params[:p_tpo].blank?
+  end
+
+  def cordinate_rating_ransack
+    p_cordinate_rating = Cordinate.ransack(params[:p_rating], search_key: :p_rating)
+    @p_cordinate_rating = p_cordinate_rating.result
+    @cordinate = Cordinate.where(user_id: current_user.id) if params[:p_rating].blank?
+  end
+
+  # ransackの設定------------
+
+  # Itemのransack設定------------
+
+  def all_item_tpo_search
+    item_tpo_ransack
+  end
+
+  def all_item_season_search
+    item_season_ransack
+  end
+
+  def all_item_color_search
+    item_color_ransack
+  end
+
+  def all_item_rating_search
+    item_rating_ransack
+  end
+
+  def all_item_super_item_search
+    item_super_item_ransack
+  end
+
+  def item_ransack_setup
+    item_tpo_ransack
+    item_rating_ransack
+    item_season_ransack
+    item_super_item_ransack
+    item_color_ransack
+  end
+
+  def item_season_ransack
+    q_item_season = Item.ransack(params[:q_season], search_key: :q_season)
+    @q_item_season = q_item_season.result
+    @item = Item.where(user_id: current_user.id) if params[:q_season].blank?
+  end
+
+  def item_tpo_ransack
+    q_item_tpo = Item.ransack(params[:q_tpo], search_key: :q_tpo)
+    @q_item_tpo = q_item_tpo.result
+    @item = Item.where(user_id: current_user.id) if params[:q_tpo].blank?
+  end
+
+  def item_rating_ransack
+    q_item_rating  = Item.ransack(params[:q_rating], search_key: :q_rating)
+    @q_item_rating = q_item_rating.result
+    @item = Item.where(user_id: current_user.id) if params[:q_rating].blank?
+  end
+
+  def item_color_ransack
+    q_item_color = Item.ransack(params[:q_color], search_key: :q_color)
+    @q_item_color = q_item_color.result
+    @item = Item.where(user_id: current_user.id) if params[:q_color].blank?
+  end
+
+  def item_super_item_ransack
+    q_item_super_item = Item.ransack(params[:q_super_item], search_key: :q_super_item)
+    @q_item_super_item = q_item_super_item.result
+    @item = Item.where(user_id: current_user.id) if params[:q_super_item].blank?
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :picture)
+  end
+
+  def sign_in(_user)
+    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
+  end
+
+  # ログイン済みユーザーかどうか確認
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = 'Please log in.'
+      redirect_to login_url
+    end
+  end
+
+  def owner_user
+    @user = User.find(params[:id])
+    @owner_user = user
+    flash[:danger] = 'オーナーしか編集できません.' unless owner_user?(@user)
+  end
+
+  # 管理者かどうか確認
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def check_guest
+    redirect_to root_path, alert: 'ゲストユーザーは削除・編集できません。' if current_user.email == 'guest@example.com'
+  end
+
+  def user_paginate
+    @users = User.all.page(params[:page])
+  end
 end
+
+# Sessoins_controllerの　current_userメソッド
+# 記憶トークンcookieに対応するユーザーを返す
+# def current_user
+#   if (user_id = session[:user_id])
+#     @current_user ||= User.find_by(id: user_id)
+#   elsif (user_id = cookies.signed[:user_id])
+#     user = User.find_by(id: user_id)
+#     if user && user.authenticated?(cookies[:remember_token])
+#       log_in user
+#       @current_user = user
+#     end
+#   end
+# end
