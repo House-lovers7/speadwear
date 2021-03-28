89a4d720492e24463decd824502df4c0626d5f4c
house-lovers7 <taisuke.gt@gmail.com>
2021-03-28 11:43:53 +0900

    change environment.rb aws_secret_access_key setting

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ if Rails.env.production?
       provider: 'AWS',
       region: ENV['S3_REGION'],  # S3に設定したリージョン。
       aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY']
     }
     config.fog_directory = ENV['S3_BUCKET']
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,6 +9,7 @@ CarrierWave.configure do |config|
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = 'speadwear-photo' # バケット名
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/speadwear-photo'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
