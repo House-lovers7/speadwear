f27597a13700e962890f2c9cc6ee3bdd85951cb4
house-lovers7 <taisuke.gt@gmail.com>
2021-04-09 22:00:57 +0900

    S3バケットの変更

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,15 +5,15 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境の場合はS3へアップロード
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory = ENV['S3_BUCKET_NAME'] # バケット名
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/speadwear-photo'
     config.fog_public = false
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキー
+      aws_access_key_id: ENV['S3_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['S3_SECRET_ACCESS_KEY'],
       region: 'ap-northeast-1',
       path_style: true
@@ -22,4 +22,16 @@ CarrierWave.configure do |config|
     config.storage :file
     config.enable_processing = false if Rails.env.test?
   end
+end
+
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET'],
+      :endpoint               => "https://s3.amazonaws.com",
+      :region                 => ENV['S3_REGION'] 
+  }
+  config.fog_directory  = ENV['S3_BUCKET']
 end
