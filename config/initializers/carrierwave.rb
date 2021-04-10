3a032da46aef2e4c8a3614fa3f9cd45a1ada4222
house-lovers7 <taisuke.gt@gmail.com>
2021-04-10 11:11:14 +0900

    carrierwaveの設定変更2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -23,15 +23,3 @@ CarrierWave.configure do |config|
     config.enable_processing = false if Rails.env.test?
   end
 end
-
-
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET'],
-      :endpoint               => "https://s3.amazonaws.com",
-      :region                 => ENV['S3_REGION'] 
-  }
-  config.fog_directory  = ENV['S3_BUCKET']
-end
