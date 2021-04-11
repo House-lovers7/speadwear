3b18d079778343b9aace8d9d3461a3baf4d13ceb
house-lovers7 <taisuke.gt@gmail.com>
2021-04-11 20:47:30 +0900

    Relationモデルのテストの大枠

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,70 +1,58 @@
+# rspec ./spec/models/relationship_spec.rb
+
 # frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  before do
-    2.times { FactoryBot.create(:user) }
+  
+  let!(:admin) { build(:admin) }
+  let!(:other) { build(:other) }
+  let!(:blockuser) { build(:blockuser) }
+  let!(:relationship1) {build(:relationship1, follower_id: admin.id, followed_id: other.id)}
+  let!(:relationship2) {build(:relationship2, follower_id: admin.id, followed_id: blockuser.id)}
+  let!(:relationship3) {build(:relationship3, follower_id: other.id, followed_id: blockuser.id)}  
+
+  it "is valid with test data" do           
+    expect(relationship1).to be_valid
   end
 
-  # 有効なファクトリを持つこと
-  it 'has a valid factory' do
-    FactoryBot.create(:relationship)
-    expect do
-      FactoryBot.create(:relationship)
-    end.to change(Relationship.all, :count).by(1)
-  end
-
-  # follower_id、followed_idの存在
-  describe 'presence of follower_id, followed_id' do
-    # follower_id、followed_idの両方があれば有効な状態であること
-    it 'is valid with a followed_id and follower_id' do
-      relationship = Relationship.new(followed_id: User.first.id,
-                                      follower_id: User.second.id)
-      expect(relationship).to be_valid
+    it 'follower_idがなければ無効な状態であること' do
+      relationship1.follower_id = nil   
+      relationship1.valid?
+      expect(relationship1.errors[:follower_id]).to include('を入力してください')
     end
 
-    # follower_idがなければ無効な状態であること
-    it 'is invalid without a follower_id' do
-      relationship = Relationship.new(followed_id: User.first.id,
-                                      follower_id: nil)
-      relationship.valid?
-      expect(relationship.errors[:follower_id]).to include('を入力してください')
+    it 'followed_idがなければ無効な状態であること' do
+      relationship1.followed_id = nil                                     
+      relationship1.valid?
+      expect(relationship1.errors[:followed_id]).to include('を入力してください')
     end
-
-    # followed_idがなければ無効な状態であること
-    it 'is invalid without a followed_id' do
-      relationship = Relationship.new(followed_id: nil,
-                                      follower_id: User.first.id)
-      relationship.valid?
-      expect(relationship.errors[:followed_id]).to include('を入力してください')
-    end
+  
+  it '自分自身をフォローできないこと' do
+    relationship1.followed_id = admin.id      
+    relationship1.valid?
+    expect(relationship1.errors[:followed_id]).to include('自分自身をフォローすることはできません')
   end
 
-  # 自分自身をフォローできないこと
-  it 'can not follow yourself' do
-    relationship = Relationship.new(followed_id: User.first.id,
-                                    follower_id: User.first.id)
-    relationship.valid?
-    expect(relationship.errors[:followed_id]).to include('自分自身をフォローすることはできません')
-  end
 
-  # 同じ人を2回以上フォローできないこと
-  it 'can not follow same person twice' do
-    Relationship.create(followed_id: User.first.id, follower_id: User.second.id)
-    relationship = Relationship.new(followed_id: User.first.id,
-                                    follower_id: User.second.id)
+  it '同じ人を2回以上フォローできないこと' do
+
+    relationship = Relationship.new(followed_id: admin.id,
+                                    follower_id: other.id)
     relationship.valid?
     expect(relationship.errors[:followed_id]).to include('はすでに存在します')
   end
 
-  # 作成と削除ができること
-  it 'can create and destroy' do
-    expect do
-      FactoryBot.create(:relationship)
+
+  it '作成と削除ができること' do    
+    
+    relationship1 = Relationship.new(follower_id: admin.id, followed_id: other.id)
+    
+    expect do          
     end.to change(Relationship.all, :count).by(1)
     expect do
-      Relationship.first.destroy
+      relationship1.destroy
     end.to change(Relationship.all, :count).by(-1)
   end
 end
