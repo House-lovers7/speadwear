2cfa703a1289905a005143884e717b452dd30c96
house-lovers7 <taisuke.gt@gmail.com>
2021-04-25 10:15:11 +0900

    コーディネートモデルのテスト

--- spec/models/cordinate_spec.rb
+++ spec/models/cordinate_spec.rb
@@ -1,91 +1,95 @@
 
 # rspec ./spec/models/cordinate_spec.rb
+#frozen_string_literal: true
 
 require 'rails_helper'
 
 RSpec.describe Cordinate, type: :model do
 
-  let!(:user) { build(:user) }
-  let!(:admin) { build(:admin) }
-  let!(:other) { build(:other) }
-  let!(:blockuser) { build(:blockuser) }
-  
+  let!(:user) { create(:user) }
+  let!(:admin) { create(:admin) }
+  let!(:item1) {build(:item1, user_id: admin.id, cordinate_id: cordinate1.id)}  
   let!(:cordinate1) {build(:cordinate1, user_id: admin.id)}
   let!(:cordinate2) {build(:cordinate2, user_id: admin.id)}
-  let!(:cordinate4) {build(:cordinate4, user_id: user.id)}
-  let!(:cordinate5) {build(:cordinate5, user_id: user.id)}
 
-  let!(:item1) {build(:item1, user_id: admin.id, cordinate_id: cordinate4.id)}
-  let!(:item2) {build(:item2, user_id: admin.id, cordinate_id: cordinate4.id)}
+  let!(:item1) {build(:item1, user_id: admin.id, cordinate_id: cordinate1.id)}
+  let!(:item2) {build(:item2, user_id: admin.id, cordinate_id: cordinate1.id)}
 
   let!(:comment1)  {build(:comment1, user_id: user.id, cordinate_id: cordinate1.id)}
-  let!(:comment2)  {build(:comment2, user_id: user.id, cordinate_id: cordinate2.id)}
-  let!(:likecordiante1) {build(:likecordinate1, user_id: user.id, cordinate_id: cordinate1.id)}
-  let!(:likecordiante2) {build(:likecordinate2, user_id: user.id, cordinate_id: cordinate2.id)}
+  let!(:comment2)  {build(:comment2, user_id: user.id, cordinate_id: cordinate1.id)}
   
-  # 存在性チェック
-  describe "test of presence" do
-    before do
-      @valid_cordinate = FactoryBot.build(:cordinate9, user_id: user.id, cordinate_id: id)
+  let!(:likecordiante1) {build(:likecordinate1, user_id: user.id, cordinate_id: cordinate1.id)}
+  
+  
+  describe "存在性チェック" do
+    #SCCESS!!
+    it "buildデータが有効であること " do
+      expect(cordinate1).to be_valid
     end
 
-    # buildデータが有効であること
-    it "is valid with a user_id and item_id, content " do
-      expect(@valid_cordinate).to be_valid
-    end
-
- # seasonがなければ無効な状態であること
- it "is invalid without a season" do
-  @valid_cordinate.season = nil
-  @valid_cordinate.valid?
-  expect(@valid_cordinate.errors[:season]).to include("を入力してください")
+#SCCESS!!
+ it "seasonがなければ無効な状態であること" do
+  cordinate1.season = nil
+  cordinate1.valid?
+  expect(cordinate1.errors[:season]).to include("を入力してください")
 end
 
- # tpoがなければ無効な状態であること
- it "is invalid without a tpo" do
-  @valid_cordinate.tpo = nil
-  @valid_cordinate.valid?
-  expect(@valid_cordinate.errors[:tpo]).to include("を入力してください")
+#SCCESS!!
+ it "tpoがなければ無効な状態であること" do
+  cordinate1.tpo = nil
+  cordinate1.valid?
+  expect(cordinate1.errors[:tpo]).to include("を入力してください")
 end
 
  # ratingがなければ無効な状態であること
+ #FAILER!!! デフォルトの設定があるからか？？
  it "is invalid without a rating" do
-  @valid_cordinate.rating = nil
-  @valid_cordinate.valid?
-  expect(@valid_cordinate.errors[:rating]).to include("を入力してください")
+  cordinate1.rating = nil
+  cordinate1.valid?
+  expect(cordinate1.errors[:rating]).to include("を入力してください")
 end
 
-  # メモの長さ
-  describe "length of memo" do
-    # 141文字のメモは無効であること
-    it "is invalid with a title which has over 51 characters" do
-      cordinate.memo = "あ" * 141
-      cordinate.valid?
-      expect(cordinate.errors[:memo]).to include("は140文字以内で入力してください")
+  #SCCESS!!
+  describe "メモの長さ" do
+    
+    it "141文字のメモは無効であること" do
+      cordinate1.memo = "あ" * 141
+      cordinate1.valid?
+      expect(cordinate1.errors[:memo]).to include("は140文字以内で入力してください")
     end
 
-    # 140文字のメモは有効であること
-    it "is valid with a title which has 140 characters" do
-      cordinate.title = "あ" * 140
-      expect(cordinate).to be_valid
-    end
-  end
-
-#ここから
-
-  # 削除の依存関係
-  describe "dependent: destoy" do
-    # 削除すると、紐づくcommentも全て削除されること
-    it "destroys all eaten_item when deleted" do
-      expect { cordinarte.destroy }.to change(Comment.all, cordinate_id: cordinate9.id :count).by(-2)
-    end
-
-    # 削除すると、紐づくlikecordinateも全て削除されること
-    it "destroys all likecordinate when deleted" do
-      2.times { FactoryBot.create(:likecordinate, cordinate_id: cordinate.id ) }
-      expect { Cordinate.destroy }.to change(Likecordinagte.all, :count).by(-2)
+    it "140文字のメモは有効であること" do
+      cordinate1.title = "あ" * 140
+      expect(cordinate1).to be_valid
     end
   end
+  
+  describe "削除の依存関係" do
 
-
+before do
+  cordinate1.save
 end
+    
+    it "削除すると、紐づくcommentも全て削除されること" do
+      cordinate1.save
+      comment1.save  
+      comment2.save
+      expect do
+        delete cordinate_delete_path(cordinate1)
+      end.to change(Comment.all, :count).by(-2)
+    end
+    
+    it "削除すると、紐づくlikecordinateも全て削除されること" do
+      likecordiante1.save
+      expect do
+        delete cordinate_delete_path(cordinate1)
+      end.to change(Likecordinate.all, :count).by(-1)
+    end       
+end
+end
+end
+
+
+
+# cordinate_delete_path	DELETE	/users/:user_id/cordinates/:id(.:format)	
+# cordinates#destroy
