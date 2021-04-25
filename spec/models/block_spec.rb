6796aeae502484b71110dbeefd2e714565bf8311
house-lovers7 <taisuke.gt@gmail.com>
2021-04-25 12:50:57 +0900

    ブロックモデルのテスト

--- spec/models/block_spec.rb
+++ spec/models/block_spec.rb
@@ -7,61 +7,59 @@ RSpec.describe Block, type: :model do
   let!(:admin) { build(:admin) }
   let!(:other) { build(:other) }
   let!(:blockuser) { build(:blockuser) }
-
   let(:block1){FactoryBot.build(:block1, blocker_id: admin.id, blocked_id: blockuser.id)}
 
 
-  before do
-    2.times { FactoryBot.create(:user) }
+  #SUCCESS!!
+  it '有効なファクトリを持つこと' do    
+    expect(block1).to be_valid
   end
 
-  # 有効なファクトリを持つこと
-  it 'has a valid factory' do
-    FactoryBot.create(:block)
-    expect { FactoryBot.create(:block) }.to change(Block.all, :count).by(1)
-  end
-
-  # blocker_id、blocked_idの存在
-  describe 'presence of blocker_id, blocked_id' do
-    # blocker_id、blocked_idの両方があれば有効な状態であること
-    it 'is valid with a blocked_id and blocker_id' do
-      block = Block.new(blocked_id: User.first.id, blocker_id: User.second.id)
-      expect(block).to be_valid
+  describe 'blocker_id、blocked_idの存在' do      
+    #SUCCESS!!
+    it 'blocker_id、blocked_idの両方があれば有効な状態であること' do      
+      expect(block1).to be_valid
     end
-
-    # blocker_idがなければ無効な状態であること
-    it 'is invalid without a blocker_id' do
-      block = Block.new(blocked_id: User.first.id, blocker_id: nil)
-      block.valid?
-      expect(block.errors[:blocker_id]).to include('を入力してください')
+    #SUCCESS!!
+    it 'blocker_idがなければ無効な状態であること' do
+      block1.blocker_id = nil      
+      block1.valid?
+      expect(block1.errors[:blocker_id]).to include('を入力してください')
     end
-
-    # blocked_idがなければ無効な状態であること
-    it 'is invalid without a blocked_id' do
-      block = Block.new(blocked_id: nil, blocker_id: User.first.id)
-      block.valid?
-      expect(block.errors[:blocked_id]).to include('を入力してください')
+    
+    #SUCCESS!!
+    it 'blocked_idがなければ無効な状態であること' do
+      block1.blocked_id = nil      
+      block1.valid?
+      expect(block1.errors[:blocked_id]).to include('を入力してください')
     end
   end
 
-  # 自分自身をブロックできないこと
-  it 'can not block yourself' do
-    block = Block.new(blocked_id: User.first.id, blocker_id: User.first.id)
-    block.valid?
-    expect(block.errors[:blocked_id]).to include('自分自身をブロックすることはできません')
+  #FAILER!!
+  it '自分自身をブロックできないこと' do
+    block1.blocked_id = admin.id           
+    block1.valid?
+    expect(block1.errors[:blocked_id]).to include('自分自身をブロックすることはできません')
   end
 
-  # 同じ人を2回以上ブロックできないこと
-  it 'can not block same person twice' do
+  #FAILER!!
+  it '同じ人を2回以上ブロックできないこと' do
     Block.create(blocked_id: User.first.id, blocker_id: User.second.id)
     block = Block.new(blocked_id: User.first.id, blocker_id: User.second.id)
     block.valid?
     expect(block.errors[:blocked_id]).to include('はすでに存在します')
   end
 
-  # 作成と削除ができること
-  it 'can create and destroy' do
-    expect { FactoryBot.create(:block) }.to change(Block.all, :count).by(1)
-    expect { Block.first.destroy }.to change(Block.all, :count).by(-1)
+  #FAILER!!
+  fit '作成と削除ができること' do
+    # expect { FactoryBot.create(:block) }.to change(Block.all, :count).by(1)   
+    expect do
+      post blockers_user_path(admin)
+    end.to change(User, :count).by(1)
+
+    expect do
+      delete blockers_user_path(admin)
+    end.to change(User, :count).by(-1)
+  
   end
 end
