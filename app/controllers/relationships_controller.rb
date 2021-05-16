0ae6eb747f06560f4832f0bf89cb95d41f988da7
house-lovers7 <taisuke.gt@gmail.com>
2021-05-16 16:34:11 +0900

    フォローアンフォロー機能の改良

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class RelationshipsController < ApplicationController
-  def create
-    if can? :create, @relationships
+  def create    
       user = User.find(params[:followed_id])
       current_user.follow(user)
       # フォロー通知機能
@@ -10,14 +9,11 @@ class RelationshipsController < ApplicationController
       redirect_to user
       # format.html { redirect_to @user }
       # format.js
-    end
-  end
+    end  
 
-  def destroy
-    if can? :destroy, @relationships
+  def destroy    
       user = Relationship.find(params[:id]).followed
       current_user.unfollow(user)
       redirect_to user
-    end
-  end
+    end  
 end
