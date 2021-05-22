d7791f55209c051a5bca34e40f7c7c9fd324caa8
house-lovers7 <taisuke.gt@gmail.com>
2021-05-22 16:03:19 +0900

    Corditeのアイテムへのdependent: :destroyを解除

--- app/models/cordinate.rb
+++ app/models/cordinate.rb
@@ -2,7 +2,7 @@
 
 class Cordinate < ApplicationRecord
   belongs_to :user, optional: true
-  has_many :items, dependent: :destroy
+  has_many :items
   has_many :comments, dependent: :destroy
   has_many :notifications, dependent: :destroy
   has_many :likecordinates, dependent: :destroy
