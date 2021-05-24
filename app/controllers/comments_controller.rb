ec778cff5e29aa99d39feae42472eebcc4d0635b
house-lovers7 <taisuke.gt@gmail.com>
2021-05-24 17:02:07 +0900

    コメント通知機能の改善

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -31,15 +31,21 @@ class CommentsController < ApplicationController
   end
 
   # コメント通知機能の実装
-  def create_notification_comment(current_user, user_id, id)
-    # 自分を除き、コメントしている人をすべて取得し、全員に通知を送る
-    temp_ids = Comment.select(:user_id).where(cordinate_id: id).where.not(user_id: current_user.id).distinct
-    temp_ids.each do |temp_id|
-      save_notification_comment(current_user, user_id, temp_id['user_id'])
-    end
-    # まだ誰もコメントしていない場合は、投稿者に通知を送る
-    save_notification_comment(current_user, user_id, id) if temp_ids.blank?
-  end
+  def create_notification_comment(current_user, user_id, id)    
+    temp = Notification.where(['sender_id = ? and receiver_id = ? and cordinate_id = ? and action = ? ',
+    current_user.id, @cordinate.user_id, id, 'comment'])
+# いいねされていない場合のみ、通知レコードを作成
+if temp.blank?
+notification = current_user.active_notifications.new(
+cordinate_id: id,
+receiver_id: @cordinate.user_id,
+action: 'comment'
+)
+# 自分の投稿に対するいいねの場合は、通知済みとする
+notification.checked = true if notification.sender_id == notification.receiver_id
+notification.save if notification.valid?
+end
+end
 
 
   def save_notification_comment(current_user, id, user_id)
